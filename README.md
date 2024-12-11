87172fd904a9d461b10438be7dc55660e7ced680
Silas Bartha <silas@exvacuum.dev>
2024-12-11 18:40:02 -0500

    Upgraded to Bevy 0.15

--- README.md
+++ README.md
@@ -3,7 +3,6 @@
 [![Crates](https://img.shields.io/crates/v/bevy_terminal_display)](https://crates.io/crates/bevy_terminal_display)
 ![License](https://img.shields.io/badge/license-0BSD%2FMIT%2FApache-blue.svg)
 ![Tag](https://img.shields.io/github/v/tag/exvacuum/bevy_terminal_display)
-![Build](https://img.shields.io/github/actions/workflow/status/exvacuum/bevy_terminal_display/rust.yml)
 [![Docs](https://img.shields.io/website?url=https%3A%2F%2Fexvacuum.github.io%2Fbevy_terminal_display%2F&label=docs)](https://exvacuum.github.io/bevy_terminal_display)
 
 A (very experimental) plugin for the [Bevy](https://bevyengine.org) engine which allows for rendering to a terminal window.
@@ -11,7 +10,7 @@ A (very experimental) plugin for the [Bevy](https://bevyengine.org) engine which
 WARNING: I highly recommend using the kitty terminal emulator with this, not only due to the gpu-accelerated rendering, but also the implementation of the kitty protocol which enables the advanced input detection.
 
 Features Include:
-- `TerminalDisplayBundle` automatically sets up a correctly-formatted render texture
+- `TerminalDisplay` automatically sets up a correctly-formatted render texture
 - Post-process dithers colors to pure black and white, which are then printed as braille characters to the terminal
 - Responsiveness to terminal window resizing
 - `TerminalInput` resource which keeps track of pressed & released keys
@@ -20,11 +19,12 @@ Features Include:
 - Log redirection
 
 ## Screenshots
-![](./doc/screenshot.png)
+![](https://git.exvacuum.dev/bevy_terminal_display/plain/doc/screenshot.png)
 ## Compatibility
 
 | Crate Version | Bevy Version |
 |---            |---           |
+| 0.5           | 0.15         |
 | 0.3-0.4       | 0.14         |
 | 0.2           | 0.13         |
 
@@ -33,13 +33,13 @@ Features Include:
 ### crates.io
 ```toml
 [dependencies]
-bevy_terminal_display = "0.3"
+bevy_terminal_display = "0.5"
 ```
 
 ### Using git URL in Cargo.toml
 ```toml
 [dependencies.bevy_terminal_display]
-git = "https://github.com/exvacuum/bevy_terminal_display.git"
+git = "https://git.exvacuum.dev/bevy_terminal_display"
 ```
 
 ## Example Usage
@@ -56,24 +56,15 @@ fn main() {
             ScheduleRunnerPlugin::run_loop(Duration::from_secs_f32(1.0 / 60.0)),
             bevy_terminal_display::TerminalDisplayPlugin::default(),
         ))
-        .insert_resource(Msaa::Off) // For post-process
         .run();
 }
 ```
 
 When spawning a camera:
 ```rs
-let terminal_display_bundle = bevy_terminal_display::display::components::TerminalDisplayBundle::new(3, &asset_server);
-
 commands.spawn((
-    Camera3dBundle {
-        camera: Camera {
-            target: terminal_display_bundle.image_handle().into(),
-            ..Default::default()
-        },
-        ..Default::default()
-    },
-    terminal_display_bundle,
+    // Camera3d...
+    TerminalDisplay(3), // Field is level of dithering
 ));
 ```
 
--- src/display/components.rs
+++ src/display/components.rs
@@ -1,72 +1,63 @@
-use bevy::{prelude::*, render::render_resource::{Extent3d, TextureDescriptor, TextureDimension, TextureFormat, TextureUsages}};
+use bevy::{
+    ecs::{
+        component::ComponentId,
+        world::DeferredWorld,
+    },
+    prelude::*,
+    render::render_resource::{
+        Extent3d, TextureDescriptor, TextureDimension, TextureFormat, TextureUsages,
+    },
+};
 use bevy_dither_post_process::components::DitherPostProcessSettings;
-use bevy_headless_render::{components::{HeadlessRenderBundle, HeadlessRenderDestination}, render_assets::HeadlessRenderSource};
+use bevy_headless_render::components::HeadlessRenderSource;
 
 /// Marker component for terminal display
-#[derive(Component)]
-pub struct TerminalDisplay;
+#[derive(Component, Debug)]
+#[component(on_add = on_add_terminal_display)]
+pub struct TerminalDisplay(pub u32);
 
-/// Bundle for terminal display, contains a handle to an image to be used as a render target to
-/// render to the terminal
-#[derive(Bundle)]
-pub struct TerminalDisplayBundle {
-    _terminal_display: TerminalDisplay,
-    _headless_render_bundle: HeadlessRenderBundle,
-    _dither_post_process_settings: DitherPostProcessSettings,
-    image_handle: Handle<Image>,
-}
+fn on_add_terminal_display(mut world: DeferredWorld, entity: Entity, _id: ComponentId) {
+    let asset_server = world.get_resource::<AssetServer>().unwrap();
+    let dither_level = world.entity(entity).get::<TerminalDisplay>().unwrap().0;
 
-impl TerminalDisplayBundle {
-    /// Create a new terminal display with the given dither level. A higher level exponentially
-    /// increases the size of the bayer matrix used in the ordered dithering calculations. If in
-    /// doubt, 3 is a good starting value to test with.
-    pub fn new(dither_level: u32, asset_server: &AssetServer) -> Self {
-        let terminal_size = crossterm::terminal::size().unwrap();
-        let size = Extent3d {
-            width: (terminal_size.0 as u32) * 2,
-            height: (terminal_size.1 as u32) * 4,
-            depth_or_array_layers: 1,
-        };
+    let terminal_size = crossterm::terminal::size().unwrap();
+    let size = Extent3d {
+        width: (terminal_size.0 as u32) * 2,
+        height: (terminal_size.1 as u32) * 4,
+        depth_or_array_layers: 1,
+    };
 
-        let mut image = Image {
-            texture_descriptor: TextureDescriptor {
-                label: None,
-                size,
-                dimension: TextureDimension::D2,
-                format: TextureFormat::R8Unorm,
-                mip_level_count: 1,
-                sample_count: 1,
-                usage: TextureUsages::TEXTURE_BINDING
-                    | TextureUsages::COPY_SRC
-                    | TextureUsages::RENDER_ATTACHMENT,
-                view_formats: &[],
-            },
-            ..default()
-        };
+    let mut image = Image {
+        texture_descriptor: TextureDescriptor {
+            label: None,
+            size,
+            dimension: TextureDimension::D2,
+            format: TextureFormat::R8Unorm,
+            mip_level_count: 1,
+            sample_count: 1,
+            usage: TextureUsages::TEXTURE_BINDING
+                | TextureUsages::COPY_SRC
+                | TextureUsages::RENDER_ATTACHMENT,
+            view_formats: &[],
+        },
+        ..default()
+    };
 
-        image.resize(size);
-        let image_handle = asset_server.add(image);
+    image.resize(size);
+    let image_handle = asset_server.add(image);
 
-        let framebuffer_extract_source =
-            asset_server.add(HeadlessRenderSource(image_handle.clone()));
-
-        Self {
-            _terminal_display: TerminalDisplay,
-            _headless_render_bundle: HeadlessRenderBundle {
-                source: framebuffer_extract_source,
-                dest: HeadlessRenderDestination::default(),
-            },
-            image_handle,
-            _dither_post_process_settings: DitherPostProcessSettings::new(
-                dither_level,
-                asset_server,
-            ),
-        }
-    }
-
-    /// Retrieves the handle to this display's target image. Anything written here will be
-    /// displayed.
-    pub fn image_handle(&self) -> Handle<Image> {
-        self.image_handle.clone()
+    let headless_render_source = HeadlessRenderSource::new(&asset_server, image_handle.clone());
+    let post_process_settings = DitherPostProcessSettings::new(dither_level, &asset_server);
+    world
+        .commands()
+        .entity(entity)
+        .insert((headless_render_source, post_process_settings));
+    if let Some(mut camera) =  world.entity_mut(entity).get_mut::<Camera>() {
+        camera.target = image_handle.into();
+    } else {
+        world.commands().entity(entity).insert(Camera {
+            target: image_handle.into(),
+            ..Default::default()
+        });
     }
 }
